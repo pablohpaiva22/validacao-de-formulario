30ea0b48029e2caaae29a570425a2c090ff272b6
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-03-23 23:07:29 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const $form = document.querySelector('.form')
+const $email = document.querySelector('input[type="email"]')
+const $name = document.querySelector('input[type="text"]')
+const $password = document.querySelector('input[type="password"]')
+const $checkbox = document.querySelector('input[type="checkbox"]')
+const $emailValidation = document.querySelector('.emailValidation')
+const $nameValidation = document.querySelector('.nameValidation')
+const $passValidation = document.querySelector('.passValidation')
+const $checkboxValidation = document.querySelector('.checkboxValidation')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e)
+    
+    if($email.value == '') $emailValidation.classList.add('ativo')
+    else $emailValidation.classList.remove('ativo')
+
+    if($name.value == '') $nameValidation.classList.add('ativo')
+    else $nameValidation.classList.remove('ativo')
+
+    if($password.value == '') $passValidation.classList.add('ativo')
+    else $passValidation.classList.remove('ativo')
+
+    if($checkbox.checked == false) $checkboxValidation.classList.add('ativo')
+    else $checkboxValidation.classList.remove('ativo')
+}
